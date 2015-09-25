3867a722a4e80e2e0b4eb38faa157da48cdb9efd
TobiasAhnoff <tobias.ahnoff@gmail.com>
2015-09-25 16:56:30 +0200

    Add DAL Migrations

---
+++ src/OpKoKo152/ResourceServer.DAL/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResourceServer.DAL.Models;
+
+namespace ResourceServer.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+ 
---
+++ src/OpKoKo152/ResourceServer.DAL/Migrations/201509251452183_Initial.cs
@@ -0,0 +1,26 @@
+namespace ResourceServer.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Value",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Value");
+        }
+    }
+}
