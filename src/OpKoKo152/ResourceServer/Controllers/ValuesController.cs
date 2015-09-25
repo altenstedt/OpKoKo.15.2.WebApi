de9295fdd1c34c82019fa71cd9c15b76fe4009d3
TobiasAhnoff <tobias.ahnoff@gmail.com>
2015-09-25 16:23:55 +0200

    Add ValueController

---
+++ src/OpKoKo152/ResourceServer/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ResourceServer.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    [RoutePrefix("api/values")]
+    public class ValuesController:ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public string Read()
+        {
+            return "Hello";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Update([FromBody] string value)
+        {
+            //TODO: Update value
+            return value;
+        }
+    }
+}
---
+++ src/OpKoKo152/ResourceServer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace ResourceServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("Api", "{Controller}");
+            config.EnableCors();
+            app.UseWebApi(config);
+        }
+    }
+}
